1612a5964cf5c38dedf86d2a21dcc15bf78e4615
Marwan Tamer Abdelmoneim <113218917+MarwanTamerMo@users.noreply.github.com>
2023-01-05 10:10:21 +0200

    Add files via upload

---
+++ Assuit C. Simple Calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	long long a, b; //Two Numbers
+
+	cin >> a >> b;
+
+	cout << a << " + " << b << " = " << a + b << endl;
+
+	cout << a << " * " << b << " = " << a * b << endl;
+
+	cout << a << " - " << b << " = " << a - b << endl;
+
+	return 0;
+
+}
