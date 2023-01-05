665dc2b4d000262048df9bb41191c7b767905609
Marwan Tamer Abdelmoneim <113218917+MarwanTamerMo@users.noreply.github.com>
2023-01-05 10:11:49 +0200

    Add files via upload
    Difference between two operations

---
+++ Assuit B-Differnce.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n1, n2, n3, n4,x;
+
+	cin >> n1 >> n2 >> n3 >> n4;
+
+	x = (n1 * n2) - (n3 * n4);
+
+	cout << "Difference = " << x;
+
+	return 0;
+
+}
