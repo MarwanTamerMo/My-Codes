ba8d0ad77194d0682bf4af503875fa7d4663a74c
Marwan Tamer Abdelmoneim <113218917+MarwanTamerMo@users.noreply.github.com>
2023-01-05 10:06:36 +0200

    Add files via upload

---
+++ Assuit B. Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	long long l;
+	float f;
+	double d;
+	char c;
+
+	cin >> n >> l >> c >> f >> d;
+	cout << n << endl << l << endl << c << endl << f << endl << d;
+
+
+	return 0;
+}
