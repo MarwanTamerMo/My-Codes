9fd77c5345a867455970fbafa0f73c6fd0048a27
Marwan Tamer Abdelmoneim <113218917+MarwanTamerMo@users.noreply.github.com>
2023-01-09 15:36:26 +0200

    Add files via upload
    Finding max number within an array using built-in function

---
+++ Assuit SH2 E. Max ( Solution 2 ).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm> // To use max_element() function
+
+using namespace std;
+
+int main() {
+
+	int arr[100], size;
+
+	cin >> size;
+
+	for (int i = 0; i < size; ++i) {
+
+		cin >> arr[i];
+	}
+
+	// Declaring a pointer to store the adress of the return of max_element()
+
+	// max_element function iterates the array elements then find the max number
+	int* max = max_element(arr, arr + size);
+											
+
+	cout << *max << endl;
+
+	return 0;
+}
